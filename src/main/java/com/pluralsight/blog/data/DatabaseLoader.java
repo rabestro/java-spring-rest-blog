3603ba9d405a950ba373e76459ee287beb296c27
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-12-16 14:17:15 +0200

    Save Database Data

--- src/main/java/com/pluralsight/blog/data/DatabaseLoader.java
+++ src/main/java/com/pluralsight/blog/data/DatabaseLoader.java
@@ -36,5 +36,6 @@ public class DatabaseLoader implements ApplicationRunner {
             Post post = new Post(title, "Lorem ipsum dolor sit amet, consectetur adipiscing elit… ");
             randomPosts.add(post);
         });
+        postRepository.saveAll(randomPosts);
     }
 }
