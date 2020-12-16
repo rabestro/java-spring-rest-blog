07357e425d858690e6a95ca303437ee269f2c173
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-12-16 14:21:41 +0200

    Add Search by Title Containing

--- src/main/java/com/pluralsight/blog/data/PostRepository.java
+++ src/main/java/com/pluralsight/blog/data/PostRepository.java
@@ -3,4 +3,8 @@ package com.pluralsight.blog.data;
 import com.pluralsight.blog.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PostRepository extends JpaRepository<Post, Long> {}
+import java.util.List;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+    List<Post> findByTitleContaining(String title);
+}
